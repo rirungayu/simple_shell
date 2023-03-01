5ded85b043ade26ff43a6fc54d1d78550ab11c19
rirungayu <rirungayu@gmail.com>
2023-03-01 05:37:55 -0800

    comman line interpreter

---
+++ exec.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * main - executes a program
+ * Return: zero
+ **/
+
+int main(int argc, char **argv)
+{
+	char *arg[] = {"/bin/ls", "-l", "/usr/", NULL};
+	if (execve(arg[0],arg, NULL) == -1)
+		perror("error");
+
+	return(0);
+}
