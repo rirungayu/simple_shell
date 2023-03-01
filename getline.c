177c3eceea538fe236dd6c074c3c15b280173508
rirungayu <38273760+rirungayu@users.noreply.github.com>
2023-03-01 16:41:45 +0300

    Update getline.c

--- getline.c
+++ getline.c
@@ -4,10 +4,12 @@
 
 /**
  * main - prints the command that was input
+ * @argc: number of arguments
+ * @argv: array of arguments
  * Return: zero
  **/
 
-int main(void)
+int main(int argc, char **argv )
 {
 	char *buffer = malloc(1024);
 	size_t len = 1024;
