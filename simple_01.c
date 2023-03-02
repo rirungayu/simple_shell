719fa1d7c47ec2ce20a41ecca3afe7f165fc8041
rirungayu <38273760+rirungayu@users.noreply.github.com>
2023-03-02 14:07:10 +0300

    Update simple_01.c

--- simple_01.c
+++ simple_01.c
@@ -10,13 +10,18 @@
 int main(int argc, char **argv)
 {
 	char command[] = "/usr/bin/ls";
-	
 	char *arg[] = {"cd",NULL};
 	char *env[] = {NULL};
+	char *buffer = malloc(1024);
+	size_t len = 1024;
 
 	(void) argc;
-	(void) argv;	
+	(void) argv;
+	printf("getline$ ");
+	getline(&buffer, &len, stdin);
+	printf("%s", strcat(command, buffer));
 	execve(command, arg, env);
+
 	return(0);
 }
 
